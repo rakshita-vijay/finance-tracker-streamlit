60707a57b21eb9b06bc8f4db9d589016f34fc82e
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-03 21:58:52 +0530

    Update p3_Generate_Report.py

--- pages/p3_Generate_Report.py
+++ pages/p3_Generate_Report.py
@@ -5,10 +5,12 @@ def generate_report_button():
     st.page_link("pages/p3_Generate_Report.py", label="📝 Generate Report") 
 
 st.header("📝 Generate AI Report")
+
 st.divider()
+
 if st.button("Generate Report"):
     md_path = gen_report()
     st.success("Report generated!")
     st.write(f"Markdown report saved at: `{md_path}`")
     with open(md_path, "r") as f:
-        st.markdown(f.read())
+        st.markdown(f.read()) 
