26a10f30b24bc15e6f379be38747da8d882dd041
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-03 20:41:49 +0530

    Update Main_Menu.py

--- Main_Menu.py
+++ Main_Menu.py
@@ -9,6 +9,7 @@ from pages.p3_Generate_Report import generate_report_button
 from pages.p4_Change_Budget import change_budget_button
 from pages.p5_Download_Files import download_files_button
 from pages.p6_Wipe_Transactions import wipe_transactions_button
+from pages.p7_Cleanup import cleanup_page_button
 
 st.set_page_config(
     page_title="Finance Tracker",
@@ -36,3 +37,4 @@ generate_report_button()
 change_budget_button()
 download_files_button()
 wipe_transactions_button()
+cleanup_page_button()
