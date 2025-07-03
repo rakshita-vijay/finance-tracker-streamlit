357f19e50b26c34925d180be2caab1564acee68e
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-04 01:31:23 +0530

    Update p3_Generate_Report.py

--- pages/p3_Generate_Report.py
+++ pages/p3_Generate_Report.py
@@ -1,5 +1,9 @@
 import time
 import streamlit as st
+
+from file_methods.md_file_methods import find_md_file_location
+from download_to_device import download_file
+
 from crewai_toolkits_gem_2point0_flash.generate_report_from_csv import gen_report 
  
 def generate_report_button():
@@ -14,13 +18,18 @@ if st.button("Generate Report"):
     with st.spinner("Generating your report..."): 
         try:
             md_path = gen_report()
-            time.sleep(3)
-            
-            # st.success("Report generated!")
-            # st.write(f"Markdown report saved at: `{md_path}`")
-            # st.divider()
+            time.sleep(3) 
             
             with open(md_path, "r") as f:
                 st.markdown(f.read())
         except Exception as e:
             st.error(f"Error during report generation: {e}") 
+            
+st.divider()
+if st.button("Download Report"):
+    with st.spinner("Downloading your report..."):
+        try:
+            md_path = find_md_file_location()
+            download_file(md_path)
+        except Exception as e:
+            st.error(f"Error during report download: {e}") 
