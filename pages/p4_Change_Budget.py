e42ae28ba8fc80c47a57af09fa4b28808de1727b
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-03 20:12:40 +0530

    Update and rename 4_Change_Budget.py to p4_Change_Budget.py

--- pages/p4_Change_Budget.py
+++ pages/p4_Change_Budget.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from core.budget_methods import changeBudget, get_budgets_list, displayBudget
 
+def change_budget_button():
+    st.page_link("pages/4_Change_Budget.py", label="💰 Change Budget")
+
 st.header("💰 Change Budget")
 budgets = get_budgets_list()
 st.write("**Current Budgets:**")
