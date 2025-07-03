7f0e563d951a71cd09ba227063979a640e39615f
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-03 22:07:50 +0530

    Update p3_Generate_Report.py

--- pages/p3_Generate_Report.py
+++ pages/p3_Generate_Report.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from crewai_toolkits_gem_2point0_flash.generate_report_from_csv import gen_report
+from utils.git_utils import git_push_md
  
 def generate_report_button():
     st.page_link("pages/p3_Generate_Report.py", label="📝 Generate Report") 
@@ -20,3 +21,4 @@ if st.button("Generate Report"):
     
     with open(md_path, "r") as f:
         st.markdown(f.read()) 
+    git_push_md()
