8ee810bb2b8c07b7b7c6989a5a9416520a76795e
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-04 00:55:43 +0530

    Update md_file_methods.py

--- file_methods/md_file_methods.py
+++ file_methods/md_file_methods.py
@@ -1,4 +1,4 @@
-import os
+import os, glob
 
 def find_md_file_location():
   curr_md = ""
@@ -9,4 +9,18 @@ def find_md_file_location():
         curr_md = ''.join(os.path.join(os.getcwd(), os.path.join(folders, file)).split('./'))
         break
 
-  return curr_md
+  return curr_md 
+
+def delete_old_md_reports(saved_files_dir="saved_files"):
+    # Find all .md files in the directory
+    md_files = glob.glob(os.path.join(saved_files_dir, "*.md"))
+    if not md_files:
+        return
+    # Sort files by modification time, newest last
+    md_files.sort(key=os.path.getmtime, reverse=True)
+    # Keep the newest file, delete the rest
+    for old_file in md_files[1:]:
+        try:
+            os.remove(old_file)
+        except Exception as e:
+            print(f"Could not delete {old_file}: {e}") 
