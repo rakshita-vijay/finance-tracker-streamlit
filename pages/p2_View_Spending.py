0333e58ca3e1575acb3b968d4cec6f3fd5a7a56e
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-03 20:10:56 +0530

    Update and rename 2_View_Spending.py to p2_View_Spending.py

--- pages/p2_View_Spending.py
+++ pages/p2_View_Spending.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from core.budget_methods import get_budgets_list, displayBudget
 from file_methods.csv_file_methods import display_csv_content
+ 
+def view_spending_button():
+    st.page_link("pages/2_View_Spending.py", label="📊 View Spending") 
 
 st.header("📊 View Spending")
 budgets = get_budgets_list()
