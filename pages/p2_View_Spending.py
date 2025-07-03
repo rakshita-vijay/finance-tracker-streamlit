37a95bc8ff79c85c9ce8ad9a4f4c0cf58b33713b
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-03 21:51:12 +0530

    Update p2_View_Spending.py

--- pages/p2_View_Spending.py
+++ pages/p2_View_Spending.py
@@ -32,31 +32,4 @@ def view_spending_page():
 
     st.table(rows)
 
-view_spending_page()
-
-
-
-'''import streamlit as st
-from core.budget_methods import get_budgets_list, displayBudget
-from file_methods.csv_file_methods import display_csv_content
- 
-def view_spending_button():
-    st.page_link("pages/p2_View_Spending.py", label="📊 View Spending") 
-
-def view_spending_page():
-    st.header("📊 View Spending")
-    
-    st.divider()
-    view_spend = st.button("View Spending")
-    st.divider()
-    
-    if view_spend:
-        budgets = get_budgets_list()
-        st.write("**Current Budgets:**")
-        displayBudget(budgets)
-        st.write("---")
-        st.write("**Transactions to Date:**")
-        display_csv_content() 
-
-view_spending_page()
-'''
+view_spending_page() 
