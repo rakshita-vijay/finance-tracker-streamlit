cae4b74867bda1f4cea43a087b5c34ea58cf0374
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-03 20:17:09 +0530

    Update p2_View_Spending.py

--- pages/p2_View_Spending.py
+++ pages/p2_View_Spending.py
@@ -3,7 +3,7 @@ from core.budget_methods import get_budgets_list, displayBudget
 from file_methods.csv_file_methods import display_csv_content
  
 def view_spending_button():
-    st.page_link("pages/2_View_Spending.py", label="📊 View Spending") 
+    st.page_link("pages/p2_View_Spending.py", label="📊 View Spending") 
 
 st.header("📊 View Spending")
 budgets = get_budgets_list()
