6ea2c8a927e1241825a27b4eaff18069bd6b802a
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-03 20:33:57 +0530

    Update p2_View_Spending.py

--- pages/p2_View_Spending.py
+++ pages/p2_View_Spending.py
@@ -5,10 +5,11 @@ from file_methods.csv_file_methods import display_csv_content
 def view_spending_button():
     st.page_link("pages/p2_View_Spending.py", label="📊 View Spending") 
 
-st.header("📊 View Spending")
-budgets = get_budgets_list()
-st.write("**Current Budgets:**")
-displayBudget(budgets)
-st.write("---")
-st.write("**Transactions to Date:**")
-display_csv_content()
+def view_spending_page():
+  st.header("📊 View Spending")
+  budgets = get_budgets_list()
+  st.write("**Current Budgets:**")
+  displayBudget(budgets)
+  st.write("---")
+  st.write("**Transactions to Date:**")
+  display_csv_content() 
