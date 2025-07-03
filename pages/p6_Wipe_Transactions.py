65161a1d16603c9dd20da4b07c8fd9812d6d750c
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-03 20:13:44 +0530

    Update and rename 6_Wipe_Transactions.py to p6_Wipe_Transactions.py

--- pages/p6_Wipe_Transactions.py
+++ pages/p6_Wipe_Transactions.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from file_methods.csv_file_methods import find_csv_file_location
 
+def wipe_transactions_button():
+    st.page_link("pages/6_Wipe_Transactions.py", label="🗑️ Wipe Transactions")
+
 st.header("🗑️ Wipe Transactions")
 if st.button("Wipe All Transactions"):
     csv_path = find_csv_file_location()
