5eade202b05379c303e577d17dc4527e03661d6a
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-03 22:06:23 +0530

    Update git_utils.py

--- utils/git_utils.py
+++ utils/git_utils.py
@@ -2,6 +2,7 @@ import os
 import git
 import streamlit as st
 from file_methods.csv_file_methods import find_csv_file_location
+from file_methods.md_file_methods import find_md_file_location
 
 def setup_git_repo():
     try:
@@ -25,5 +26,18 @@ def git_push_csv(csv_relative_path=find_csv_file_location(), commit_message="Upd
         origin = repo.remote(name='origin')
         origin.push()
         return True, "CSV pushed to GitHub successfully."
+    except Exception as e:
+        return False, f"Git push failed: {e}"
+
+def git_push_md(md_relative_path=find_md_file_location(), commit_message="Update markdown report via Streamlit"):
+    repo = setup_git_repo()
+    if repo is None:
+        return False, "Git repo not found."
+    try:
+        repo.git.add(md_relative_path)
+        repo.index.commit(commit_message)
+        origin = repo.remote(name='origin')
+        origin.push()
+        return True, f"Markdown report pushed to GitHub successfully."
     except Exception as e:
         return False, f"Git push failed: {e}"
