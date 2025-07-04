21ea5f3b936a3fadb026d02ab2c4f8a62f7a7183
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-04 21:18:03 +0530

    Update p6_Wipe_Transactions.py

--- pages/p6_Wipe_Transactions.py
+++ pages/p6_Wipe_Transactions.py
@@ -1,15 +1,18 @@
 import streamlit as st
 from file_methods.csv_file_methods import find_csv_file_location
+from utils.git_utils import git_push_csv
 
 def wipe_transactions_button():
     st.page_link("pages/p6_Wipe_Transactions.py", label="🗑️ Wipe Transactions") 
     
 def wipe_transactions_page():
     st.header("🗑️ Wipe Transactions")
+    st.divider()
     if st.button("Wipe All Transactions"):
         csv_path = find_csv_file_location()
         with open(csv_path, "w") as f:
             f.write("S.NO,DATE,DESCRIPTION,AMOUNT,PAYMENT METHOD,STATUS,NOTES\n")
         st.success("All transactions wiped!")
+    git_push_csv()
 
 wipe_transactions_page()
