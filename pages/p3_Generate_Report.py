ab9a3b9dfddcfd653f4161139c1475b650f908f2
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-05 00:36:10 +0530

    Update p3_Generate_Report.py

--- pages/p3_Generate_Report.py
+++ pages/p3_Generate_Report.py
@@ -15,6 +15,17 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 ) 
 
+with st.sidebar:
+    st.markdown("## Main Menu")
+    st.page_link("app.py", label="Home", icon="🏠")
+    st.page_link("pages/p1_Add_Transactions.py", label="Add Transactions", icon="➕")
+    st.page_link("pages/p2_View_Spending.py", label="View Spending", icon="📊")
+    st.page_link("pages/p3_Generate_Report.py", label="Generate Report", icon="📝")
+    st.page_link("pages/p4_Change_Budget.py", label="Change Budget", icon="💰")
+    st.page_link("pages/p5_Download_Files.py", label="Download Files", icon="⬇️")
+    st.page_link("pages/p6_Wipe_Transactions.py", label="Wipe Transactions", icon="🗑️")
+    st.page_link("pages/p7_Cleanup.py", label="Cleanup", icon="🧹")  
+
 def generate_report_button():
     st.page_link("pages/p3_Generate_Report.py", label="📝 Generate Report") 
 
