7cee81b7ec506dd5e684597468d14a555ea0f68f
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-04 21:53:12 +0530

    Update p3_Generate_Report.py

--- pages/p3_Generate_Report.py
+++ pages/p3_Generate_Report.py
@@ -18,29 +18,31 @@ st.set_page_config(
 def generate_report_button():
     st.page_link("pages/p3_Generate_Report.py", label="📝 Generate Report") 
 
-st.header("📝 Generate AI Report")
-
-st.divider()
-
-md_path = "saved_files/md_report.md"
-
-if st.button("Generate Report"): 
+def generate_report_page():
+    st.header("📝 Generate AI Report") 
     st.divider()
-    with st.spinner("Generating your report..."): 
-        try:
-            md_path = gen_report() 
-            with open(md_path, "r") as f:
-                st.markdown(f.read())
-        except Exception as e:
-            st.error(f"Error during report generation: {e}") 
-            
-git_push_md(md_path)
-st.divider()            
-if os.path.isfile(md_path): 
-    with open(md_path, "rb") as f:
-        if st.download_button(
-            label="Download Report",
-            data=f,
-            file_name=os.path.basename(md_path),
-            mime="text/markdown"):
-            st.success(f"\nDownload of file: {os.path.basename(md_path)} complete! Check your downloads folder :)")  
+    
+    md_path = "saved_files/md_report.md"
+    
+    if st.button("Generate Report"): 
+        st.divider()
+        with st.spinner("Generating your report..."): 
+            try:
+                md_path = gen_report() 
+                with open(md_path, "r") as f:
+                    st.markdown(f.read())
+            except Exception as e:
+                st.error(f"Error during report generation: {e}") 
+                
+    git_push_md(md_path)
+    st.divider()            
+    if os.path.isfile(md_path): 
+        with open(md_path, "rb") as f:
+            if st.download_button(
+                label="Download Report",
+                data=f,
+                file_name=os.path.basename(md_path),
+                mime="text/markdown"):
+                st.success(f"\nDownload of file: {os.path.basename(md_path)} complete! Check your downloads folder :)") 
+
+generate_report_page()
