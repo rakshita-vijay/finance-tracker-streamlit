82c00178d053926e7efeb18a2d862b73cf8eab16
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-05 17:00:23 +0530

    Update p1_Add_Transactions.py

--- pages/p1_Add_Transactions.py
+++ pages/p1_Add_Transactions.py
@@ -13,13 +13,14 @@ st.set_page_config(
 with st.sidebar:
     st.markdown("## Main Menu")
     st.page_link("Main_Menu.py", label="Home", icon="🏠")
+    st.page_link("pages/p0_Authentication.py", label="Authentication", icon="🔐")
     st.page_link("pages/p1_Add_Transactions.py", label="Add Transactions", icon="➕")
     st.page_link("pages/p2_View_Spending.py", label="View Spending", icon="📊")
     st.page_link("pages/p3_Generate_Report.py", label="Generate Report", icon="📝")
     st.page_link("pages/p4_Change_Budget.py", label="Change Budget", icon="💰")
     st.page_link("pages/p5_Download_Files.py", label="Download Files", icon="⬇️")
     st.page_link("pages/p6_Wipe_Transactions.py", label="Wipe Transactions", icon="🗑️")
-    st.page_link("pages/p7_Cleanup.py", label="Cleanup", icon="🧹") 
+    st.page_link("pages/p7_Cleanup.py", label="Cleanup", icon="🧹")  
 
 def add_transactions_button():
     st.page_link("pages/p1_Add_Transactions.py", label="➕ Add Transactions")
