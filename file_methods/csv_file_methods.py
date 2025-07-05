5fdd3c9b787efbb7d21c6fd152f542065b5fc323
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-05 19:08:48 +0530

    Update csv_file_methods.py

--- file_methods/csv_file_methods.py
+++ file_methods/csv_file_methods.py
@@ -3,6 +3,8 @@ from crewai_toolkits_gem_2point0_flash.transform_csv_to_md_table import transfor
 
 from file_methods.user_file_utils import get_user_file
 
+import streamlit as st
+
 if 'username' not in st.session_state:
     st.warning("Please login first.")
     st.stop()
