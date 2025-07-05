b70938360df95b0f31db2975a24cc365e1f194f1
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-05 22:59:56 +0530

    Update Main_Menu.py

--- Main_Menu.py
+++ Main_Menu.py
@@ -2,7 +2,11 @@ __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3') 
 
-import streamlit as st
+import streamlit as st 
+
+if 'username' not in st.session_state:
+    st.switch_page("pages/p0_Authentication.py")
+
 from pages.p0_Authentication import authentication_button
 from pages.p1_Add_Transactions import add_transactions_button
 from pages.p2_View_Spending import view_spending_button
