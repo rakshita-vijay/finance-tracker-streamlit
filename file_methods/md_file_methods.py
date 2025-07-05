2c40f7d49640361f32cdcb0b0de0e4fd964c630a
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-05 17:25:07 +0530

    Update md_file_methods.py

--- file_methods/md_file_methods.py
+++ file_methods/md_file_methods.py
@@ -1,15 +1,10 @@
 import os, glob
 
-def find_md_file_location():
-  curr_md = ""
+from file_methods.user_file_utils import get_user_file
 
-  for folders, _, files in os.walk("./saved_files"):
-    for file in files:
-      if file[len(file)-3 : len(file)] == '.md':
-        curr_md = ''.join(os.path.join(os.getcwd(), os.path.join(folders, file)).split('./'))
-        break
-
-  return curr_md  
+def find_md_file_location(): 
+  md_path = get_user_file(username, "md_report.md")
+  return md_path 
 
 def save_and_cleanup_md_report(md_content, timestamp, md_filename="md_report.md", saved_files_dir="saved_files"):
   md_path = os.path.join(saved_files_dir, md_filename)
