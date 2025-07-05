c9e2865a5633f6b2d56c6f105ad5558874b44645
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-05 23:20:16 +0530

    Update p7_Cleanup.py

--- pages/p7_Cleanup.py
+++ pages/p7_Cleanup.py
@@ -1,7 +1,6 @@
 import streamlit as st
 if 'username' not in st.session_state:
-    st.warning("Please login first.")
-    st.stop()
+    st.switch_page("pages/p0_Authentication.py")
 username = st.session_state['username']
 
 from download_to_device import cleanup_pycache_and_temp_files
