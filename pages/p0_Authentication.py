0a38c867f6eb77491ba891a2c47376dccbf2f10a
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-05 23:16:22 +0530

    Update p0_Authentication.py

--- pages/p0_Authentication.py
+++ pages/p0_Authentication.py
@@ -87,16 +87,4 @@ def authentication_page():
                 st.rerun()
                 ensure_user_dir(username)
  
-authentication_page() 
-
-with st.sidebar:
-    st.markdown("## Main Menu")
-    st.page_link("Main_Menu.py", label="Home", icon="🏠")
-    st.page_link("pages/p0_Authentication.py", label="Authentication", icon="🔐")
-    st.page_link("pages/p1_Add_Transactions.py", label="Add Transactions", icon="➕")
-    st.page_link("pages/p2_View_Spending.py", label="View Spending", icon="📊")
-    st.page_link("pages/p3_Generate_Report.py", label="Generate Report", icon="📝")
-    st.page_link("pages/p4_Change_Budget.py", label="Change Budget", icon="💰")
-    st.page_link("pages/p5_Download_Files.py", label="Download Files", icon="⬇️")
-    st.page_link("pages/p6_Wipe_Transactions.py", label="Wipe Transactions", icon="🗑️")
-    st.page_link("pages/p7_Cleanup.py", label="Cleanup", icon="🧹") 
+authentication_page()
