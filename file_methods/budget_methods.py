fe0d17d324813383452aceff34e27aaf5929662a
Rakshita Vijay <rakshita.vijay1@gmail.com>
2025-07-06 20:33:37 +0530

    indent lol

--- file_methods/budget_methods.py
+++ file_methods/budget_methods.py
@@ -1,6 +1,6 @@
 import streamlit as st
 if 'username' not in st.session_state:
-    st.switch_page("pages/p0_Authentication.py")
+  st.switch_page("pages/p0_Authentication.py")
 username = st.session_state['username']
 
 import os, sys, re, math
