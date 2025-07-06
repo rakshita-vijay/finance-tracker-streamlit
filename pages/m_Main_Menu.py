26f79df77d5a93a873ab85b8a29151f8dcce2b86
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-06 13:25:55 +0530

    Update m_Main_Menu.py

--- pages/m_Main_Menu.py
+++ pages/m_Main_Menu.py
@@ -1,6 +1,7 @@
 import streamlit as st
 if 'username' not in st.session_state:
     st.switch_page("pages/p0_Authentication.py") 
+username = st.session_state['username']
 
 from pages.p0_Authentication import authentication_button
 from pages.p1_Add_Transactions import add_transactions_button
