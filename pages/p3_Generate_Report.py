1cc78498064fb6021e78104515ece4ee5596111d
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-06 17:52:01 +0530

    Update p3_Generate_Report.py

--- pages/p3_Generate_Report.py
+++ pages/p3_Generate_Report.py
@@ -1,7 +1,9 @@
 import streamlit as st
 if 'username' not in st.session_state:
   st.switch_page("pages/p0_Authentication.py")
-username = st.session_state['username']
+username = st.session_state['username']  
+
+from pages.p0_Authentication import create_empty_files(username)
 create_empty_files(username)
 
 import os, time
