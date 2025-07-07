a371de8c104391641008e7a835591284e03e82b5
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-08 00:18:45 +0530

    Update p4_Change_Budget.py

--- pages/p4_Change_Budget.py
+++ pages/p4_Change_Budget.py
@@ -15,17 +15,8 @@ st.set_page_config(
   initial_sidebar_state="expanded"
 )
 
-with st.sidebar:
-  st.markdown("## Main Menu")
-  st.page_link("Main_Menu.py", label="Home", icon="🏠")
-  st.page_link("pages/p0_Authentication.py", label="Authentication", icon="🔐")
-  st.page_link("pages/p1_Add_Transactions.py", label="Add Transactions", icon="➕")
-  st.page_link("pages/p2_View_Spending.py", label="View Spending", icon="📊")
-  st.page_link("pages/p3_Generate_Report.py", label="Generate Report", icon="📝")
-  st.page_link("pages/p4_Change_Budget.py", label="Change Budget", icon="💰")
-  st.page_link("pages/p5_Download_Files.py", label="Download Files", icon="⬇️")
-  st.page_link("pages/p6_Wipe_Transactions.py", label="Wipe Transactions", icon="🗑️")
-  st.page_link("pages/p7_Cleanup.py", label="Cleanup", icon="🧹")
+from the_sidebar import the_sb
+the_sb() 
 
 def change_budget_button():
   st.page_link("pages/p4_Change_Budget.py", label="💰 Change Budget")
