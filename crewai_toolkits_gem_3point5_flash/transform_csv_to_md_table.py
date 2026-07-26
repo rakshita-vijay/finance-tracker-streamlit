f9dbaa8f17cd07e395b585d5194b2fdec756243b
Rakshita Vijay <rakshita.vijay1@gmail.com>
2026-07-26 17:41:50 +0530

    added bun back to req

--- crewai_toolkits_gem_3point5_flash/transform_csv_to_md_table.py
+++ crewai_toolkits_gem_3point5_flash/transform_csv_to_md_table.py
@@ -1,6 +1,6 @@
-# __import__('pysqlite3')
-# import sys
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import os, sys, csv
 from crewai import Agent, Task, Crew, LLM
